8b4eeeb523df70a99c6b14570fac79cd3ec6d04a
han190 <limpid.mazarine@gmail.com>
2023-01-19 14:55:38 -0500

    upload refined image

--- src/icon.f90
+++ src/icon.f90
@@ -5,7 +5,10 @@ program main
 
    type(point_type), allocatable :: piles(:), curve(:), temp(:)
    type(point_type) :: point, center
-   integer :: n, u
+   integer :: n, u, i, j
+   character(:), allocatable :: fmt(:)
+   character(:), allocatable :: msg
+   real :: bdy
    real :: x(3, 3), y(3, 3), c(2)
    namelist /parameters/ n, c, x, y
 
@@ -55,47 +58,68 @@ program main
 
    call execute_command_line("mkdir -p ./data/")
    open (newunit=u, file='./data/plot_icon.plt')
-   write (u, *) "set terminal png size 1000, 500 transparent truecolor"
-   write (u, *) "set size 1, 1"
-   write (u, *) "set key noautotitle"
-   write (u, *) "light_color = '#6d5192'"
-   write (u, *) "dark_color = '#d79921'"
-   write (u, *) "color = light_color"
-   write (u, *) "set xrange [-3.5:3.5]"
-   write (u, *) "set yrange [-3.5:3.5]"
-   write (u, *) "set style line 1 linewidth 3 linecolor rgb dark_color"
-   write (u, *) "set style line 2 linewidth 3 linecolor rgb light_color"
-   write (u, *) "set tics format ''"
-   write (u, *) "set pixmap 1 './data/f1954.png' at first -3, -3 size first 6, 6 behind"
-   write (u, "('set xtics', 1x, '(', 9(f6.2, ','), tl1, ')')") x
-   write (u, "('set ytics', 1x, '(', 9(f6.2, ','), tl1, ')')") y
+   write (u, "(a)") "set terminal png size 750, 750 transparent truecolor"
+   write (u, "(a)") "set key noautotitle"
+   write (u, "(a)") "light_color = '#6d5192'"
+   write (u, "(a)") "dark_color = '#d79921'"
+   write (u, "(a)") "color = dark_color"
+   write (u, "(a)") "set xrange [-3.5:3.5]"
+   write (u, "(a)") "set yrange [-3.5:3.5]"
+   write (u, "(a)") "set style line 1 linewidth 3 linecolor rgb color"
+   write (u, "(a)") "set style line 2 linewidth 1 linecolor rgb color linetype 0"
+   write (u, "(a)") "set style line 3 linewidth 1.5 linecolor rgb color linetype 0"
+   write (u, "(a)") "set tics format ''"
+   ! write (u, "(a)") "set pixmap 1 './data/f1954.png' at first -3, -3 size first 6, 6 behind"
 
-   write (u, *) "set output './data/icon_dark.png'"
-   write (u, *) "set multiplot layout 1, 2"
-   write (u, *) "set border linestyle 1"
-   write (u, *) "set grid linecolor rgb dark_color"
-   write (u, *) "plot './data/icon.dat' with lines linestyle 1"
-   ! write (u, *) "unset grid"
-   write (u, *) "set tics scale 0"
-   write (u, *) "plot './data/icon.dat' with lines linestyle 1"
-   write (u, *) "unset multiplot"
+   write (u, "(a)") "set output './data/icon_dark.png'"
+   write (u, "(a)") "unset border"
+   write (u, "(a)") "set tics scale 0"
 
-   write (u, *) "set output './data/icon_light.png'"
-   write (u, *) "set multiplot layout 1, 2"
-   write (u, *) "set border linestyle 2"
-   write (u, *) "set grid linecolor rgb light_color"
-   write (u, *) "plot './data/icon.dat' with lines linestyle 2"
-   ! write (u, *) "unset grid"
-   write (u, *) "set tics scale 0"
-   write (u, *) "plot './data/icon.dat' with lines linestyle 2"
-   write (u, *) "unset multiplot"
+   allocate (character(len=500) :: fmt(3))
+   fmt(1) = "('set label', a, 1x, 'at', f6.2, ',', f6.2, "// &
+      & "1x, 'center front textcolor rgb color')"
+   fmt(2) = "('set arrow', 1x, 2(a, 1x, f6.2, ',', f6.2, 1x), 'nohead linestyle 2')"
+   fmt(3) = "('set arrow', 1x, 2(a, 1x, f6.2, ',', f6.2, 1x), 'nohead linestyle 3')"
 
-   write (u, *) "set terminal svg size 1000, 1000 dynamic enhanced"
-   write (u, *) "set output './data/icon.svg'"
-   write (u, *) "unset pixmap"
-   write (u, *) "unset border"
-   write (u, *) "unset tics"
-   write (u, *) "plot './data/icon.dat' with lines linestyle 2"
+   allocate (character(len=9) :: msg)
+   do j = 1, 3
+      do i = 1, 3
+
+         bdy = 3.1
+         write (u, trim(fmt(2))) 'from', x(i, j), -bdy, 'to', x(i, j), bdy
+         write (u, trim(fmt(2))) 'from', -bdy, y(i, j), 'to', bdy, y(i, j)
+         write (msg, "(a, i0, ',', i0, a)") '"x_{', i, j, '}"'
+
+         bdy = merge(-3.3, 3.3, mod((j - 1)*3 + i, 2) == 1)
+         write (u, trim(fmt(1))) msg, x(i, j), bdy
+         msg(2:2) = 'y'
+         write (u, trim(fmt(1))) msg, bdy, y(i, j)
+
+      end do
+   end do
+
+   bdy = 3.1
+   write (u, trim(fmt(3))) 'from', center%x, -bdy, 'to', center%x, bdy
+   write (u, trim(fmt(3))) 'from', -bdy, center%y, 'to', bdy, center%y
+
+   bdy = 3.3
+   msg = "'x_c'"
+   write (u, trim(fmt(1))) msg, center%x, -bdy
+   msg = "'y_c'"
+   write (u, trim(fmt(1))) msg, -bdy, center%y
+
+   write (u, "(a)") "plot './data/icon.dat' with lines linestyle 1"
+
+   write (u, "(a)") "set output './data/icon_light.png'"
+   write (u, "(a)") "color = light_color"
+   write (u, "(a)") "replot"
+
+   write (u, "(a)") "set terminal svg size 1000, 1000 dynamic enhanced"
+   write (u, "(a)") "set output './data/icon.svg'"
+   write (u, "(a)") "unset pixmap"
+   write (u, "(a)") "unset border"
+   write (u, "(a)") "unset tics"
+   write (u, "(a)") "plot './data/icon.dat' with lines linestyle 1"
    close (u)
 
    call execute_command_line("gnuplot ./data/plot_icon.plt")
